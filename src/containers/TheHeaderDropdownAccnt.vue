85f6f56998f36985e50f6e20f2bc93f8db6a74f3
atjhoendz <achunarmando@gmail.com>
2021-03-23 17:57:52 +0700

    feat(auth): implement API logout

--- src/containers/TheHeaderDropdownAccnt.vue
+++ src/containers/TheHeaderDropdownAccnt.vue
@@ -2,25 +2,45 @@
   <CDropdown inNav class="c-header-nav-items" placement="bottom">
     <template #toggler-content>
       <span class="h4 d-md-down-none" style="color: #3c4b64">
-        $store.state.user.currentUser.username
+        {{ $store.state.user.currentUser.username }}
       </span>
       <span class="h5 d-lg-none" style="color: #3c4b64">
-        $store.state.user.currentUser.username
+        {{ $store.state.user.currentUser.username }}
       </span>
     </template>
     <CDropdownItem to="/login">
       <CIcon name="cil-lock-locked" /> Logout
     </CDropdownItem>
+    <toast-msg :listToasts="listToasts" />
   </CDropdown>
 </template>
 
 <script>
+import { AuthService } from '../services/auth.service'
+import ToastMsg from '../components/ToastMsg'
+
 export default {
   name: 'TheHeaderDropdownAccnt',
+  components: {
+    ToastMsg,
+  },
   data() {
     return {
-      itemsCount: 42,
+      listToasts: [],
     }
+  },
+  methods: {
+    async makeLogout() {
+      try {
+        await AuthService.makeLogout()
+      } catch (err) {
+        const toast = {
+          message: 'Terjadi masalah. Logout tidak berhasil.',
+          color: 'danger',
+        }
+        this.listToasts.push(toast)
+      }
+    },
   },
 }
 </script>
