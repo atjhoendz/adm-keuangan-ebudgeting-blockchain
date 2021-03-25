f3b4524b88101998005c1688a6f9f0bdbae56cb4
atjhoendz <achunarmando@gmail.com>
2021-03-25 23:16:49 +0700

    feat(estimasi): implement API Get All Data

--- src/views/data-estimasi/DataEstimasi.vue
+++ src/views/data-estimasi/DataEstimasi.vue
@@ -3,44 +3,65 @@
     <CCol col="12">
       <card-list-data
         title="Data Estimasi"
-        :items="itemsEstimasi"
+        :items="items"
         :fields="fields"
         routeEndpoint="data-estimasi"
         :showEdit="false"
+        :isLoading="isLoading"
       >
       </card-list-data>
     </CCol>
+    <toast-msg :listToasts="listToasts" />
   </CRow>
 </template>
 
 <script>
-import CardListData from "../../components/CardListData.vue";
-import { itemsEstimasi } from "../../sample-data/data";
-
-const fields = [
-  { key: "no", label: "No.", _style: "width:10px" },
-  "tanggal-berangkat",
-  "nama-pemohon",
-  "dari-lembaga",
-  { key: "jenis-pmk", label: "Jenis PMK" },
-  "asal",
-  "tujuan",
-  "banyak",
-  "biaya-satuan",
-  "total",
-  { key: "proses", _style: "width:50px" },
-];
+import CardListData from '../../components/CardListData.vue'
+import ToastMsg from '../../components/ToastMsg'
+import { EstimasiService } from '../../services/estimasi.service'
+import fields from './fields'
 
 export default {
-  name: "DataEstimasi",
-  components: { CardListData },
+  name: 'DataEstimasi',
+  components: { CardListData, ToastMsg },
   data() {
     return {
-      itemsEstimasi,
       fields,
-    };
+      items: [],
+      listToasts: [],
+      isLoading: false,
+    }
   },
-};
+  methods: {
+    async getData() {
+      this.isLoading = true
+      try {
+        const data = await EstimasiService.getAll()
+
+        this.items = data.map(item => {
+          return {
+            key: item.Key,
+            ...item.Record,
+          }
+        })
+      } catch (err) {
+        const toast = {
+          message: 'Terjadi masalah. Data tidak berhasil didapatkan.',
+          color: 'danger',
+        }
+        this.listToasts.push(toast)
+      }
+      this.isLoading = false
+    },
+  },
+  async mounted() {
+    await this.getData()
+    if (this.$store.state.toast.listToasts.length) {
+      this.listToasts = this.$store.state.toast.listToasts
+      this.$store.commit('toast/RESET')
+    }
+  },
+}
 </script>
 
 <style></style>
---
+++ src/views/data-estimasi/fields.js
@@ -0,0 +1,15 @@
+const fields = [
+  { key: 'no', label: 'No.', _style: 'width:10px' },
+  { key: 'tanggal_berangkat' },
+  'nama_pemohon',
+  { key: 'nama_lembaga', label: 'Dari Lembaga' },
+  { key: 'jenis_pmk', label: 'Jenis PMK' },
+  'asal',
+  'tujuan',
+  'banyak',
+  { key: 'biaya', label: 'Biaya Satuan' },
+  'total',
+  { key: 'proses', _style: 'width:50px' },
+]
+
+export default fields
